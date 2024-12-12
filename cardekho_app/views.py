04b14678e80c040e8944b20a159cb170c8cda6bc
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2024-12-12 13:59:04 +0545

    Update views.py

--- cardekho_app/views.py
+++ cardekho_app/views.py
@@ -1,19 +1,49 @@
+from urllib import request
+
+import status as status
 from django.shortcuts import render
 from .models import car_list
 from django.http import JsonResponse
 from .api_file.serializers import carSerializers
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
-@api_view()
+
+@api_view(['GET', 'POST'])
 def car_list_view(request):
-    car = car_list.objects.all()
-    serializer = carSerializers(car, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+      car = car_list.objects.all()
+      serializer = carSerializers(car, many=True)
+      return Response(serializer.data)
 
-
-@api_view()
+    if request.method == 'POST':
+        serializer = carSerializers(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+@api_view(['GET', 'PUT', 'DELETE'])
 def car_detail_view(request,pk):
-    car = car_list.objects.get(pk=pk)
-    serializer = carSerializers(car)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        try:
+          car = car_list.objects.get(pk=pk)
+        except:
+            return Response({'Error':'car not found'}, status= status.HTTP_404_NOT_FOUND)
+        serializer = carSerializers(car)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        car = car_list.objects.get(pk=pk)
+        serializer = carSerializers(car,data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+          return Response(serializer.errors,status=status.HTTP_400_BAD_RESQUEST)
+
+    if request.method == 'DELETE':
+        car = car_list.objects.get(pk=pk)
+        car.delete()
+        return Response (status= status.HTTP_204_NO_CONTENT)
