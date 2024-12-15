87eb8ddd098f9c75c1e8b9e76d894cbfa07c1fff
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2024-12-15 13:22:54 +0545

    Update serializers.py

--- cardekho_app/api_file/serializers.py
+++ cardekho_app/api_file/serializers.py
@@ -8,25 +8,32 @@ def alphanumeric(value):
         raise serializers.ValidationError("Only alphanumeric characters are allowed")
 
 
-class carSerializers(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    desc = serializers.CharField()
-    active = serializers.BooleanField()
-    chassisnumber = serializers.CharField(validators=[alphanumeric])
-    price = serializers.DecimalField(max_digits=9, decimal_places=2)
+# class carSerializers(serializers.Serializer):
+# id = serializers.IntegerField(read_only=True)
+# name = serializers.CharField()
+# desc = serializers.CharField()
+# active = serializers.BooleanField()
+# chassisnumber = serializers.CharField(validators=[alphanumeric])
+# price = serializers.DecimalField(max_digits=9, decimal_places=2)
 
-    def create(self, validated_data):
-        return car_list.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.desc = validated_data.get('desc', instance.desc)
-        instance.active = validated_data.get('active', instance.active)
-        instance.chassisnumber = validated_data.get('chassisnumber', instance.chassisnumber)
-        instance.price = validated_data.get('price', instance.price)
-        instance.save()
-        return instance
+# def create(self, validated_data):
+#     return car_list.objects.create(**validated_data)
+#
+# def update(self, instance, validated_data):
+#     instance.name = validated_data.get('name', instance.name)
+#     instance.desc = validated_data.get('desc', instance.desc)
+#     instance.active = validated_data.get('active', instance.active)
+#     instance.chassisnumber = validated_data.get('chassisnumber', instance.chassisnumber)
+#     instance.price = validated_data.get('price', instance.price)
+#     instance.save()
+#     return instance
+# model serializers
+class carSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = car_list
+        # exclude = ['name']
+        fields = '__all__'
+        # fields = ['id', 'account_name', 'users', 'created']
 
     def validate_price(self, value):
         if value <= 20000.00:
