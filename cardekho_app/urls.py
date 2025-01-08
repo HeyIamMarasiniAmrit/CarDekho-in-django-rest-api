ef2c0544ab2441a3e2240202e883d3cfc1b389e3
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-01-08 14:49:23 +0545

    Update urls.py

--- cardekho_app/urls.py
+++ cardekho_app/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
     path('list', views.car_list_view, name='car_list'),
-    path('<int:pk>',views.car_detail_view,name='car_detail'),
-     path('Showroom',views.Showroom_view.as_view(),name='showroom_view'),
-    # path('showroom/<int:pk>,view')
-    
-
+    path('<int:pk>', views.car_detail_view, name='car_detail'),
+    path('Showroom', views.Showroom_view.as_view(), name='showroom_view'),
+    path('Showroom/<int:pk>', views.showroom_details.as_view(), name='showroom_details'),  # Added this
+    path('showroom/<int:pk>', views.showroom_details.as_view(), name='showroom_details'),
 ]
