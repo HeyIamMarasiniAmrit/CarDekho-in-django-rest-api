5cb7d2c2b83ac19157aa3d433f8ea9bce60ca2de
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-01-12 14:15:13 +0545

    Update admin.py

--- cardekho_app/admin.py
+++ cardekho_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import car_list, Showroomlist
+from .models import car_list, Showroomlist, Review
 
 # Register your models here.
 admin.site.register(car_list)
 admin.site.register(Showroomlist)
+admin.site.register(Review)
 
