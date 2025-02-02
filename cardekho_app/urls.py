451c5ec28f4e80f455441a2fc26ac8a68aeaa2df
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-02-02 20:04:31 +0545

    Update urls.py

--- cardekho_app/urls.py
+++ cardekho_app/urls.py
@@ -1,12 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router = DefaultRouter()
+router.register('Showroom',views.Showroom_viewset, basename='showroom')
 
 urlpatterns = [
     path('list', views.car_list_view, name='car_list'),
     path('<int:pk>', views.car_detail_view, name='car_detail'),
-    path('Showroom', views.Showroom_view.as_view(), name='showroom_view'),
-    path('Showroom/<int:pk>', views.showroom_details.as_view(), name='showroom_details'),  # Added this
-    path('showroom/<int:pk>', views.showroom_details.as_view(), name='showroom_details'),
+    path('',include(router.urls)),
+    # path('Showroom', views.Showroom_view.as_view(), name='showroom_view'),
+    # path('Showroom/<int:pk>', views.showroom_details.as_view(), name='showroom_details'),  # Added this
+    # path('showroom/<int:pk>', views.showroom_details.as_view(), name='showroom_details'),
     path('review',views.Reviewlist.as_view(),name='review_list'),
     path('review/<int:pk>',views.ReviewDetails.as_view(),name='review_detail'),
 ]
