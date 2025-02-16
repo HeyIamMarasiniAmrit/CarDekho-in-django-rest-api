a7f654a49111fb0df304e16a0ce96d12a5a78a75
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-02-16 14:18:12 +0545

    Update urls.py

--- cardekho_app/urls.py
+++ cardekho_app/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
     # path('Showroom', views.Showroom_view.as_view(), name='showroom_view'),
     # path('Showroom/<int:pk>', views.showroom_details.as_view(), name='showroom_details'),  # Added this
     # path('showroom/<int:pk>', views.showroom_details.as_view(), name='showroom_details'),
-    path('review',views.Reviewlist.as_view(),name='review_list'),
-    path('review/<int:pk>',views.ReviewDetails.as_view(),name='review_detail'),
+    # path('review',views.Reviewlist.as_view(),name='review_list'),
+    # path('review/<int:pk>',views.ReviewDetails.as_view(),name='review_detail'),
+    path('showroom/<int:pk>/review-create',views.ReviewCreate.as_view(),name='review_create'),
+    path('showroom/<int:pk>/review',views.Reviewlist.as_view(),name='review_lsi'),
+    path('showroom/<int:pk>',views.ReviewDetails.as_view(),name='review_detail'),
 ]
