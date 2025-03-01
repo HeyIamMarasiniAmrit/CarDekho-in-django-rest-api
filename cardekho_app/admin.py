d667dcedfb26fee641b868854db92a4f354923bf
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-03-01 11:36:25 +0545

    Add files via upload

--- cardekho_app/admin.py
+++ cardekho_app/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import car_list, Showroomlist, Review
 
 # Register your models here.
-admin.site.register(car_list)
-admin.site.register(Showroomlist)
-admin.site.register(Review)
-
