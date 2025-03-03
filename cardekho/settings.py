6a201ff103e4382c164141f195a703f760cb0332
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-03-03 13:12:51 +0545

    Update settings.py

--- cardekho/settings.py
+++ cardekho/settings.py
@@ -132,4 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':['rest_framework.authentication.SessionAuthentication'],
     'DEFAULT_PERMISSION_CLASSES':['rest_framework.permissions.IsAuthenticated'],
+    
+    'rest_framework_simplejwt.authentication.JWTAuthentication',],
 }
