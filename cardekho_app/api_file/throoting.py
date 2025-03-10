b8aaafc82c8d66681997f1811dd19b54f12b106b
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-03-10 12:48:23 +0545

    Update throoting.py

--- cardekho_app/api_file/throoting.py
+++ cardekho_app/api_file/throoting.py
@@ -1,1 +1,8 @@
 
+from rest_framework.throttling import userRateThrottle
+
+class ReviewDetailThrottle(userRateThrottle):
+    scope = 'throttling_for_review_details'
+
+class Reviewlistthrottle(userRateThrottle):
+    scope = 'throttling_for_review_list'
