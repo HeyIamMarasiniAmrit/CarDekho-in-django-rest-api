797d2797873a0f21e686ee8e0034e0515d04f76c
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-07-04 20:05:05 +0545

    Update urls.py

--- cardekho_app/urls.py
+++ cardekho_app/urls.py
@@ -1,19 +1,20 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from user_app.api.views import registration_view , logout_view
+from user_app.api.views import registration_view, logout_view
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
-    path('login/',obtain_auth_token, name='login'),
-    path('register/',registration_view, name='login'),
-    path('logout/',logout_view, name='logout'),
-    path('logout/',logout_view, name='logout'),
+    # For TokenAuthentication (optional, if you're using DRF's token auth)
+    path('login/', obtain_auth_token, name='token_login'),
+
+    # Custom Register and Logout views
+    path('register/', registration_view, name='register'),
+    path('logout/', logout_view, name='logout'),
+
+    # JWT Authentication
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-
 ]
